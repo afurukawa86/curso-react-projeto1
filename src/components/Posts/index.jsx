fe5fc6ecbc58556a9f05f8334def7ec8c53f33e9
afurukawa <afurukawa86@gmail.com>
2021-11-22 20:27:17 -0300

    Tests

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 export const Posts = ({ posts }) => (
     <div className="posts">
         {posts.map(post => (
-            <PostCard key={post.id} title={post.title} body={post.body} cover={post.cover} />
+            <PostCard key={post.id} id={post.id} title={post.title} body={post.body} cover={post.cover} />
         ))}
     </div>
 )
